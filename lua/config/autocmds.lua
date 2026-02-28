c249faccc1fc6e92bc3be5f5ab19765e7a447b0e
cdj5492 <cdj5492@rit.edu>
2026-02-28 17:08:26 -0500

    better file autosave

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,11 +7,20 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-local function write_if_writable()
-  if vim.bo.modifiable and not vim.bo.readonly and vim.bo.buftype == "" then
-    vim.cmd.wa()
-  end
-end
+-- local function write_if_writable()
+--   if vim.bo.modifiable and not vim.bo.readonly and vim.bo.buftype == "" then
+--     vim.cmd.wa()
+--   end
+-- end
+--
+-- vim.api.nvim_create_autocmd("FocusLost", { callback = write_if_writable })
+-- vim.api.nvim_create_autocmd("BufLeave", { callback = write_if_writable })
 
-vim.api.nvim_create_autocmd("FocusLost", { callback = write_if_writable })
-vim.api.nvim_create_autocmd("BufLeave", { callback = write_if_writable })
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+  group = vim.api.nvim_create_augroup("AutosaveOnLeave", { clear = true }),
+  callback = function()
+    if vim.bo.modified and vim.bo.buftype == "" and vim.fn.filereadable(vim.api.nvim_buf_get_name(0)) == 1 then
+      pcall(vim.cmd, "silent! update")
+    end
+  end,
+})
